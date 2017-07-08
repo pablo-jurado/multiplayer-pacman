bece37b992b955816b51803a3bba0be036369743
Pablo Jurado <pjurado85@gmail.com>
2017-07-08 14:09:54 -0500

    create function that captures arrow keys events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import { version } from 'inferno'
 import Component from 'inferno-component'
-import './registerServiceWorker'
 import './App.css'
+
+function move(direction) {
+  console.log('move ' + direction)
+}
+
+function checkArrow (event) {
+  const keyValue = event.keyCode
+  const left = 37
+  const up = 38
+  const right = 39
+  const down = 40
+
+  if (keyValue === left) move('left')
+  if (keyValue === right) move('right')
+  if (keyValue === up) move('up')
+  if (keyValue === down) move('down')
+}
+
+function Player () {
+  return (
+    <div className='player' />
+  )
+}
+
+function Board () {
+  return (
+    <div className='game-board'>
+      {Player()}
+    </div>
+  )
+}
 
 function App () {
   return (
-    <div>
-      <h2>
-        Multiplayer Pacman
-      </h2>
+    <div tabIndex='0' onkeydown={checkArrow} className='game'>
+      <div>
+        <h2>Multiplayer Pacman</h2>
+        {Board()}
+      </div>
     </div>
   )
 }
