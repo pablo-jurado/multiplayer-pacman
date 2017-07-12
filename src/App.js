27a863a869bf689d39bc8cc71ff351f3a328853d
Pablo Jurado <pjurado85@gmail.com>
2017-07-12 13:22:34 -0500

    update board styles

--- src/App.js
+++ src/App.js
@@ -43,8 +43,10 @@ function Player (player) {
 function Square (square) {
   let classVal = 'square'
   const squares = square.map((item, i) => {
-    if (item === 1) classVal = 'square wall'
     if (item === 0) classVal = 'square'
+    if (item === 1) classVal = 'square wall'
+    if (item === 2) classVal = 'square dot'
+
     return <div key={i} className={classVal} />
   })
   return squares
