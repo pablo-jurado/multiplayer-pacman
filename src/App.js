876dc2e6de98adad6649a86da3169ff25fb0cb4d
Pablo Jurado <pjurado85@gmail.com>
2017-07-12 12:06:11 -0500

    took player out of the board and position it absolute

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { version } from 'inferno'
 import Component from 'inferno-component'
 import './App.css'
 
+window.addEventListener('keydown', checkArrow)
+
 function checkArrow (event) {
   const keyValue = event.keyCode
   const left = 37
@@ -11,18 +13,35 @@ function checkArrow (event) {
 
   if (keyValue === left) window.appState.player.direction = 'left'
   if (keyValue === right) window.appState.player.direction = 'right'
-  if (keyValue === up) window.appState.player.direction = 'up'
-  if (keyValue === down) window.appState.player.direction = 'down'
+  if (keyValue === up) window.appState.player.direction = 'top'
+  if (keyValue === down) window.appState.player.direction = 'bottom'
 }
 
-window.addEventListener('keydown', checkArrow)
+function Player (player) {
+  let direction = player.direction
+  let speed = player.speed
+  let x = player.x
+  let y = player.y
+
+  if (direction === 'right') x += speed
+  if (direction === 'left') x -= speed
+  if (direction === 'bottom') y += speed
+  if (direction === 'top') y -= speed
+
+  let styles = { left: x + '%', top: y + '%' }
+  window.appState.player.x = x
+  window.appState.player.y = y
+
+  return (
+    <div className='player' style={styles} />
+  )
+}
 
 function Square (square) {
   let classVal = 'square'
   const squares = square.map((item, i) => {
     if (item === 1) classVal = 'square blue'
     if (item === 0) classVal = 'square'
-    if (item === 'p') classVal = 'player'
     return <div key={i} className={classVal} />
   })
   return squares
@@ -35,12 +54,11 @@ function Board (state) {
   })
   return (
     <div className='board'>
+      {Player(state.player)}
       {rows}
     </div>
   )
 }
-
-//     <div tabIndex='0' onkeydown={checkArrow} className='game'>
 
 function App (state) {
   return (
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import App from './App'
 
 let emptyBoard =
   [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-  [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
-  [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -45,36 +45,9 @@ window.appState = {
   board: emptyBoard.slice(0),
   player: {
     direction: 'right',
-    x: 1,
-    y: 1
-  }
-}
-
-function movePlayer () {
-  let x = window.appState.player.x
-  let y = window.appState.player.y
-  let board = window.appState.board
-  const direction = window.appState.player.direction
-
-  board[x][y] = 0
-
-  if (direction === 'right' && board[x][y + 1] !== 1) y++
-  if (direction === 'left' && board[x][y - 1] !== 1) y--
-  if (direction === 'down' && board[x + 1][y] !== 1) x++
-  if (direction === 'up' && board[x - 1][y] !== 1) x--
-
-  board[x][y] = 'p'
-  window.appState.player.x = x
-  window.appState.player.y = y
-
-  // this is checking for the hole in the board
-  if (y === 0) {
-    board[x][y] = 0
-    window.appState.player.y = board.length - 2
-  }
-  if (y === board.length - 2) {
-    board[x][y] = 0
-    window.appState.player.y = 0
+    speed: 0.5,
+    x: 0,
+    y: 0
   }
 }
 
@@ -82,18 +55,15 @@ function movePlayer () {
 // Render Loop
 // -----------------------------------------------------------------------------
 
-// window.setInterval(renderNow, 511)
-
 const rootEl = document.getElementById('app')
-const renderTime = 200
+const renderTime = 100
 
 function renderNow () {
-  movePlayer()
   render(App(window.appState), rootEl)
   // window.requestAnimationFrame(renderNow)
 }
 // window.requestAnimationFrame(renderNow)
 
-// setInterval(renderNow, renderTime)
+setInterval(renderNow, renderTime)
 
 renderNow()
