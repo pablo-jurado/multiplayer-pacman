cce09253d30e2f99c29d51db13d4476abbf2a7b6
Pablo Jurado <pjurado85@gmail.com>
2017-07-17 17:17:28 -0500

    fix dot bug

--- src/Tile.js
+++ src/Tile.js
@@ -3,22 +3,23 @@ import Component from 'inferno-component'
 import mori from 'mori'
 import { log } from './App'
 
-function Tile (squares) {
+function Tile (tiles) {
   let classVal = 'square'
-  const squaresNum = mori.count(squares)
-  let squaresArr = []
+  const tilesNum = mori.count(tiles)
+  let tilesArr = []
 
-  for (let i = 0; i < squaresNum; i++) {
-    const item = mori.get(squares, i)
+  for (let i = 0; i < tilesNum; i++) {
+    const item = mori.get(tiles, i)
 
     if (item === 0) classVal = 'square'
     if (item === 1) classVal = 'square wall'
     if (item === 2) classVal = 'square dot'
     if (item === 3) classVal = 'square power-dot'
+    if (typeof item === 'string') classVal = 'square'
 
-    squaresArr.push(<div key={i} className={classVal} />)
+    tilesArr.push(<div key={i} className={classVal} />)
   }
-  return squaresArr
+  return tilesArr
 }
 
 export default Tile
